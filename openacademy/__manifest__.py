9c050e071015a065650199c6fe5f2335bcf8fa6d
ThanhPT <thanhptse@gmail.com>
2017-04-10 01:24:55 +0700

    miss security

--- openacademy/__manifest__.py
+++ openacademy/__manifest__.py
@@ -27,6 +27,9 @@
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'views/openacademy.xml',
+        'views/partner.xml',
+        'views/session_workflow.xml',        
     ],
     # only loaded in demonstration mode
     'demo': [
